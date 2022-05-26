3b6a52f7c790cf5ede493b6a4b667ab409d4f4ec
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-26 13:08:26 -0300

    Criação do arquivo etapas.js, controle das áreas da tela,

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QWERT',
+                vice: 'Zul',
+                fotos:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
