2467504c67fdb9ad78a7d85fcef6c321f9296cdd
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-27 15:00:02 -0300

    adicionei a procura dos candidatos quando digitar os numeros, e também a funcionalidade de voto nulo

--- etapas.js
+++ etapas.js
@@ -4,7 +4,7 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: '38111',
                 name: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos:[
@@ -12,7 +12,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 name: 'Beltrano da Silva',
                 partido: 'DEFG',
                 fotos:[
@@ -26,7 +26,7 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: '99',
                 name: 'Ciclano',
                 partido: 'ABC',
                 vice: 'Cic',
@@ -36,7 +36,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 84,
+                numero: '84',
                 name: 'Zulano',
                 partido: 'QWERT',
                 vice: 'Zul',
--- script.js
+++ script.js
@@ -29,7 +29,30 @@ let numeros = document.querySelector('.d-1-3');
         numeros.innerHTML = numeroHtml;
     }
     function atualizaInterface(){
-        alert("Finalizou de digitar o voto!");
+        let etapa = etapas[etapaAtual];
+        let candidato = etapa.candidatos.filter((item)=>{
+            if(item.numero === numero) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        if(candidato.length > 0) {
+            candidato = candidato[0];
+            seuVotoPara.style.display = 'block';
+            aviso.style.display = 'block'
+            descricao.innerHTML = `Nome: ${candidato.name}<br/>Partido: ${candidato.partido}`;
+            let fotosHtml = '';
+            for(let i in candidato.fotos) {
+                fotosHtml += `<div class="d-1-image"><img src="imagem/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
+            lateral.innerHTML = fotosHtml;
+        } else {
+            seuVotoPara.style.display = 'block';
+            aviso.style.display = 'block'
+            descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>'
+        }
+
     }
 
 function clicou(n) {
